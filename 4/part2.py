ba7015462aaefae887fb367fe21c03b59849c49c
Renato Biassoni <r.biassoni@geniodiligence.it>
2021-12-04 22:05:32 +0100

    day 4 part 2 and missing input, requirements

---
+++ 4/part2.py
@@ -0,0 +1,62 @@
+#! /usr/bin/python3.10
+# -*- coding:utf8 -*-
+
+
+"""
+SPEC
+input: a list of numbers
++
+a set of bingo boards
+
+the last board in which all numbers in a row or column appear wins
+return its score, calculated as the sum of all uncalled numbers
+multiplied by the lst called one
+"""
+from io import StringIO
+
+import numpy as np
+
+
+def parse_bingo(game):
+    draws = game.pop(0).split(",")
+    boards = [np.loadtxt(StringIO(board), dtype=int) for board in game]
+    board_size = boards[0].shape[0]
+    return draws, boards, board_size
+
+
+def score(draw, board):
+    return int(np.nansum(board)) * int(draw)
+
+
+def is_winner(board, size):
+    for i in range(size):
+        drawn_row = np.sum(np.isnan(board[i, :]))
+        drawn_col = np.sum(np.isnan(board[:, i]))
+        if size in (drawn_row, drawn_col):
+            return True
+    return False
+
+
+def find_winner(draws, boards, board_size):
+    completed_boards = []
+    winning_draws = []
+    for count_draw, draw in enumerate(draws):
+        for count_board, board in enumerate(boards):
+            if count_board not in completed_boards:
+                board = np.where(board == int(draw), np.nan, board)
+                boards[count_board] = board
+                if count_draw >= board_size:
+                    if is_winner(board, board_size):
+                        completed_boards.append(count_board)
+                        winning_draws.append(draw)
+    return winning_draws[-1], completed_boards[-1]
+
+
+with open("input.txt") as f:
+    game = f.read().split('\n\n')
+    draws, boards, board_size = parse_bingo(game)
+    draw, winner = find_winner(draws, boards, board_size)
+    print(draw)
+    print(winner)
+    score = score(draw, boards[winner])
+    print(score)
